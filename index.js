a2ebf575504ce6d04e19267818eb0f814bef6aac
Alexander Zaretskii <al.zaretskii@gmail.com>
2016-01-20 15:44:46 +0200

    fix Oauth

--- index.js
+++ index.js
@@ -1,123 +1,24 @@
-var _ = require('lodash');
-var ig = require('instagram-node').instagram();
+var _ = require('lodash'),
+    util = require('./util.js'),
+    instagram = require('instagram-node').instagram();
 
-var globalPickResult = {
-    medias: {
-        fields: {
-            'id': 'MediaID',
-            'user.username': 'Username',
-            'images.standard_resolution.url': 'Media',
-            'caption.text': 'Caption',
-            'likes.count': 'Likes',
-            'tags': 'Tags',
-            'location': 'Location'
-        }
-    }
-};
+var pickInputs = {
+        'userId': { key: 'userId', validate: { req: true } },
+        'count': 'count',
+        'min_id': 'min_id',
+        'max_id': 'max_id'
+    },
+    pickOutputs = {
+        'id': { key: 'data', fields: ['id']},
+        'username': { key: 'data', fields: ['user.username']},
+        'standard_resolution': { key: 'data', fields: ['images.standard_resolution.url']},
+        'caption_text': { key: 'data', fields: ['caption.text']},
+        'likes': { key: 'data', fields: ['likes.count']},
+        'tags': { key: 'data', fields: ['tags']},
+        'location': { key: 'data', fields: ['location']}
+    };
 
 module.exports = {
-
-    /**
-     * Return pick result.
-     *
-     * @param output
-     * @param pickTemplate
-     * @returns {*}
-     */
-    pickResult: function (output, pickTemplate) {
-        var result = {};
-        // map template keys
-        _.map(_.keys(pickTemplate), function (templateKey) {
-
-            var oneTemplateObject = pickTemplate[templateKey];
-            var outputKeyValue = _.get(output, templateKey);
-
-            if (!outputKeyValue) {
-
-                return result;
-            }
-            // if template key is object - transform, else just save
-            if (_.isObject(oneTemplateObject)) {
-                // if data is array - map and transform, else once transform
-                if (_.isArray(outputKeyValue)) {
-
-                    result = this._mapPickArrays(outputKeyValue, oneTemplateObject);
-                } else {
-
-                    result[oneTemplateObject.key] = this.pickResult(outputKeyValue, oneTemplateObject.fields);
-                }
-            } else {
-
-                _.set(result, oneTemplateObject, outputKeyValue);
-            }
-        }, this);
-
-        return result;
-    },
-
-    /**
-     * System func for pickResult.
-     *
-     * @param mapValue
-     * @param templateObject
-     * @returns {*}
-     * @private
-     */
-    _mapPickArrays: function (mapValue, templateObject) {
-
-        var arrayResult = [],
-            result = templateObject.key? {} : [];
-
-        _.map(mapValue, function (inOutArrayValue) {
-
-            arrayResult.push(this.pickResult(inOutArrayValue, templateObject.fields));
-        }, this);
-
-        if (templateObject.key) {
-
-            result[templateObject.key] = arrayResult;
-        } else {
-
-            result = arrayResult;
-        }
-
-        return result;
-    },
-
-    /**
-     * Set acess token.
-     *
-     * @param dexter
-     */
-    authParams: function (dexter) {
-
-        if (dexter.environment('instagram_access_token')) {
-
-            ig.use({access_token: dexter.environment('instagram_access_token')});
-        } else {
-
-            this.fail('A [instagram_access_token] environment is Required.');
-        }
-    },
-
-    /**
-     * To string inputs object.
-     *
-     * @param inputs
-     * @param inputAttributes
-     * @returns {{}}
-     */
-    prepareStringInputs: function (inputs) {
-        var result = {};
-
-        _.map(inputs, function (inputValue, inputKey) {
-
-            result[inputKey] = _(inputValue).toString();
-        });
-
-        return result;
-    },
-
     /**
      * The main entry point for the Dexter module
      *
@@ -125,24 +26,18 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
-        
-        this.authParams(dexter);
+        var credentials = dexter.provider('instagram').credentials(),
+            inputs = util.pickInputs(step, pickInputs),
+            validateErrors = util.checkValidateErrors(inputs, pickInputs);
 
-        if (!step.input('userId').first()) {
+        // check params.
+        if (validateErrors)
+            return this.fail(validateErrors);
 
-            this.fail('A [userId] is Required for this module.');
-        } else {
+        instagram.use({ access_token: _.get(credentials, 'access_token') });
+        instagram.user_media_recent(inputs.userId, _.omit(inputs, 'userId'), function (error, medias) {
 
-            ig.user_media_recent(step.input('userId').first(), this.prepareStringInputs(_.omit(step.inputs(), 'userId')), function (err, medias) {
-
-                if (err) {
-
-                    this.fail(err);
-                } else {
-
-                    this.complete(this.pickResult({medias: medias}, globalPickResult));
-                }
-            }.bind(this));
-        }
+            error? this.fail(error) : this.complete(util.pickOutputs({ data: medias }, pickOutputs));
+        }.bind(this));
     }
 };
